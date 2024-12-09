06adae8e8f929858997cd68102bfe0ee5716891c
ZinurX <145262061+ZinurX@users.noreply.github.com>
2024-12-09 16:59:35 +0300

    create project

---
+++ src/test/java/edu/otib/AppTest.java
@@ -0,0 +1,12 @@
+package edu.otib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
